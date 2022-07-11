d4e3b4baeb3a5bb0ac5ae6464a83e1e2f04f4814
Omkar A. Katta <omkarakatta@gmail.com>
2022-07-11 07:53:55 -0400

    perf(review): lint

--- src/mt2018review.jl
+++ src/mt2018review.jl
@@ -1,42 +1,38 @@
+# Produce DGP for MT (2018).
 function dgp_review()
-    DGP(
-      suppZ = reshape([1, 2, 3, 4], :, 1), # reshape returns a 2-dim object
-      densZ = [0.25, 0.25, 0.25, 0.25],
-      pscore = [0.12, 0.29, 0.48, 0.78],
-      mtrs = (
-        MTR(bernstein_basis(2), hcat(0.9, 0.35, 0.1)),
-        MTR(bernstein_basis(2), hcat(0.35, 0.2, 0))
-      )
-    )
+    DGP(suppZ = reshape([1, 2, 3, 4], :, 1), # reshape returns 2-dim object
+        densZ = [0.25, 0.25, 0.25, 0.25],
+        pscore = [0.12, 0.29, 0.48, 0.78],
+        mtrs = (MTR(bernstein_basis(2), hcat(0.9, 0.35, 0.1)),
+                MTR(bernstein_basis(2), hcat(0.35, 0.2, 0))))
 end
 
+# Produce default aesthetics for MT (2018).
 function defaults_review()
-    settings = Dict(
-        :axisheight => "2in",
-        :axiswidth => "3in",
-        :fontsize => "\\large",
-        :legendcols => 4,
-        :linewidth => "1.7pt",
-        :linewidthmtr => "2.1pt",
-        # :markrepeat => "25",
-        # :markphase => "12",
-        :title => nothing,
-        :titlesuffix => "",
-        :titlevspace => "5pt",
-        :xmin => "0",
-        :xmax => "1",
-        :weightymin => "-8",
-        :weightymax => "8",
-        :mtrlegendtext => nothing,
-        :mtrymin => "-.01",
-        :mtrymax => "1.01",
-        :mtrylabel => "MTR",
-        :mteylabel => "MTE",
-        :xlabel => "\$u\$",
-        :ylabelbuffer => "1.20",
-        :ylabeltextwidth => "1in",
-        :ylabelweights =>  "Weights (where \$\\neq 0\$)"
-    )
+    settings = Dict(:axisheight => "2in",
+                    :axiswidth => "3in",
+                    :fontsize => "\\large",
+                    :legendcols => 4,
+                    :linewidth => "1.7pt",
+                    :linewidthmtr => "2.1pt",
+                    # :markrepeat => "25",
+                    # :markphase => "12",
+                    :title => nothing,
+                    :titlesuffix => "",
+                    :titlevspace => "5pt",
+                    :xmin => "0",
+                    :xmax => "1",
+                    :weightymin => "-8",
+                    :weightymax => "8",
+                    :mtrlegendtext => nothing,
+                    :mtrymin => "-.01",
+                    :mtrymax => "1.01",
+                    :mtrylabel => "MTR",
+                    :mteylabel => "MTE",
+                    :xlabel => "\$u\$",
+                    :ylabelbuffer => "1.20",
+                    :ylabeltextwidth => "1in",
+                    :ylabelweights =>  "Weights (where \$\\neq 0\$)")
 
     colors = ["gray", "teal", "orange", "cyan!60!black", "red!70!white",
               "lime!80!black", "red", "yellow!60!black", "pink",
@@ -76,24 +72,22 @@ end
 function run_k4(savedir::String, compile::Bool = false)
     dgp = dgp_review()
     bases = [(bernstein_basis(4), bernstein_basis(4))]
-    assumptions = Dict{Symbol, Any}(
-        :lb => 0,
-        :ub => 1,
-        :saturated => false,
-        :ivslope => true,
-        :tslsslopeind => true
-    );
+    assumptions = Dict{Symbol, Any}(:lb => 0,
+                                    :ub => 1,
+                                    :saturated => false,
+                                    :ivslope => true,
+                                    :tslsslopeind => true)
     opts = defaults_review()
     opts[1][:title] = "Bounds"
     opts[1][:titlesuffix] = " -- shown at upper bound"
-    texfn = mtrs_and_weights(savedir, "k4";
-        dgp = dgp,
-        tp = att(dgp),
-        bases = bases,
-        assumptions = assumptions,
-        mtroption = "max",
-        opts = opts
-    )
+    texfn = mtrs_and_weights(savedir,
+                             "k4";
+                             dgp = dgp,
+                             tp = att(dgp),
+                             bases = bases,
+                             assumptions = assumptions,
+                             mtroption = "max",
+                             opts = opts)
     if compile
         compile_latex(texfn)
     end
@@ -103,24 +97,22 @@ end
 function run_k9(savedir::String, compile::Bool = false)
     dgp = dgp_review()
     bases = [(bernstein_basis(9), bernstein_basis(9))]
-    assumptions = Dict{Symbol, Any}(
-        :lb => 0,
-        :ub => 1,
-        :saturated => false,
-        :ivslope => true,
-        :tslsslopeind => true
-    );
+    assumptions = Dict{Symbol, Any}(:lb => 0,
+                                    :ub => 1,
+                                    :saturated => false,
+                                    :ivslope => true,
+                                    :tslsslopeind => true)
     opts = defaults_review()
     opts[1][:title] = "Bounds"
     opts[1][:titlesuffix] = " -- shown at upper bound"
-    texfn = mtrs_and_weights(savedir, "k9";
-        dgp = dgp,
-        tp = att(dgp),
-        bases = bases,
-        assumptions = assumptions,
-        mtroption = "max",
-        opts = opts
-    )
+    texfn = mtrs_and_weights(savedir,
+                             "k9";
+                             dgp = dgp,
+                             tp = att(dgp),
+                             bases = bases,
+                             assumptions = assumptions,
+                             mtroption = "max",
+                             opts = opts)
     if compile
         compile_latex(texfn)
     end
@@ -140,28 +132,24 @@ function run_np(savedir::String, compile::Bool = false)
     dgp = dgp_review()
     knots = vcat(0, 1, dgp.pscore)
     bases = [(constantspline_basis(knots), constantspline_basis(knots))];
-    assumptions = Dict{Symbol, Any}(
-        :lb => 0,
-        :ub => 1,
-        :saturated => false,
-        :ivslope => true,
-        :tslsslopeind => true
-    )
+    assumptions = Dict{Symbol, Any}(:lb => 0,
+                                    :ub => 1,
+                                    :saturated => false,
+                                    :ivslope => true,
+                                    :tslsslopeind => true)
     opts = defaults_review()
     opts[1][:title] = "Bounds"
     opts[1][:titlesuffix] = " -- shown at upper bound"
-    texfn = mtrs_and_weights(savedir, "np";
-        dgp = dgp,
-        tp = att(dgp),
-        bases = bases,
-        assumptions = assumptions,
-        mtroption = "max",
-        opts = opts,
-        attributes = Dict(
-            "LogLevel" => 0,
-            "SolveType" => 1
-        )
-    )
+    texfn = mtrs_and_weights(savedir,
+                             "np";
+                             dgp = dgp,
+                             tp = att(dgp),
+                             bases = bases,
+                             assumptions = assumptions,
+                             mtroption = "max",
+                             opts = opts,
+                             attributes = Dict("LogLevel" => 0,
+                                               "SolveType" => 1))
     if compile
         compile_latex(texfn)
     end
@@ -171,25 +159,23 @@ end
 function run_k9_decr(savedir::String, compile::Bool = false)
     dgp = dgp_review()
     bases = [(bernstein_basis(9), bernstein_basis(9))]
-    assumptions = Dict{Symbol, Any}(
-        :lb => 0,
-        :ub => 1,
-        :saturated => false,
-        :ivslope => true,
-        :tslsslopeind => true,
-        :decreasing_level => [(1, 0), (1, 1)]
-    );
+    assumptions = Dict{Symbol, Any}(:lb => 0,
+                                    :ub => 1,
+                                    :saturated => false,
+                                    :ivslope => true,
+                                    :tslsslopeind => true,
+                                    :decreasing_level => [(1, 0), (1, 1)])
     opts = defaults_review()
     opts[1][:title] = "Bounds"
     opts[1][:titlesuffix] = " -- shown at upper bound"
-    texfn = mtrs_and_weights(savedir, "k9-decr";
-        dgp = dgp,
-        tp = att(dgp),
-        bases = bases,
-        assumptions = assumptions,
-        mtroption = "max",
-        opts = opts
-    )
+    texfn = mtrs_and_weights(savedir,
+                             "k9-decr";
+                             dgp = dgp,
+                             tp = att(dgp),
+                             bases = bases,
+                             assumptions = assumptions,
+                             mtroption = "max",
+                             opts = opts)
     if compile
         compile_latex(texfn)
     end
@@ -199,27 +185,25 @@ end
 function run_k9_decr_add_more(savedir::String, compile::Bool = false)
     dgp = dgp_review()
     bases = [(bernstein_basis(9), bernstein_basis(9))]
-    assumptions = Dict{Symbol, Any}(
-        :lb => 0,
-        :ub => 1,
-        :saturated => false,
-        :ivslope => true,
-        :tslsslopeind => true,
-        :wald => [(2, 4)],
-        :olsslope => true,
-        :decreasing_level => [(1, 0), (1, 1)]
-    );
+    assumptions = Dict{Symbol, Any}(:lb => 0,
+                                    :ub => 1,
+                                    :saturated => false,
+                                    :ivslope => true,
+                                    :tslsslopeind => true,
+                                    :wald => [(2, 4)],
+                                    :olsslope => true,
+                                    :decreasing_level => [(1, 0), (1, 1)])
     opts = defaults_review()
     opts[1][:title] = "Bounds"
     opts[1][:titlesuffix] = " -- shown at upper bound"
-    texfn = mtrs_and_weights(savedir, "k9-decr-add-more";
-        dgp = dgp,
-        tp = att(dgp),
-        bases = bases,
-        assumptions = assumptions,
-        mtroption = "max",
-        opts = opts
-    )
+    texfn = mtrs_and_weights(savedir,
+                             "k9-decr-add-more";
+                             dgp = dgp,
+                             tp = att(dgp),
+                             bases = bases,
+                             assumptions = assumptions,
+                             mtroption = "max",
+                             opts = opts)
     if compile
         compile_latex(texfn)
     end
